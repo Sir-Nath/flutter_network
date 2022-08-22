a27e7157d4b272112ffd323eac6973b5d9b40c19
Sir_Nath <peternathaniel49@gmail.com>
2022-08-22 19:55:02 +0100

    correction to folder structure

--- lib/data/repository/http/http_repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_network/data/model/data_model.dart';
-import 'package:flutter_network/data/provider/http/http.dart';
-
-class HttpRepo{
-  late PostModel postModel;
-  PostClient postClient = PostClient();
-}
